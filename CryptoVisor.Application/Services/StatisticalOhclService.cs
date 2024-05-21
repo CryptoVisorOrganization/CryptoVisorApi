d12a9b07a54ac7e850aa8cb5f8924ba3bc8a32de
Gabriel Simões <gabrielsimoest@gmail.com>
2024-05-21 16:24:40 -0300

    Feat: Calculo RSI para todos os periodos

--- CryptoVisor.Application/Services/StatisticalOhclService.cs
+++ CryptoVisor.Application/Services/StatisticalOhclService.cs
@@ -1,30 +1,25 @@
-﻿using CryptoVisor.Application.Commands;
-using CryptoVisor.Application.Interfaces;
+﻿using CryptoVisor.Application.Interfaces;
 using CryptoVisor.Core.Entities;
-using System;
+using CryptoVisor.Core.ValueObjects;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Channels;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CryptoVisor.Application.Services
 {
-	public class StatisticalOhclService
-	{
-		private readonly IOhlcRepository _ohlcRepository;
-		private readonly IUnitOfWork _unitOfWork;
+    public class StatisticalOhclService
+    {
+        private readonly IOhlcRepository _ohlcRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-		public StatisticalOhclService(
-			IOhlcRepository ohlcRepository,
-			IUnitOfWork unitOfWork
+        public StatisticalOhclService(
+            IOhlcRepository ohlcRepository,
+            IUnitOfWork unitOfWork
 
-			)
-		{
-			_ohlcRepository = ohlcRepository;
-			_unitOfWork = unitOfWork;
-		}
+            )
+        {
+            _ohlcRepository = ohlcRepository;
+            _unitOfWork = unitOfWork;
+        }
 
         public async Task<OhlcStatitical> GetOhlcStatitical(DateTime firstDate, DateTime lastDate, ECoinType coinType)
         {
@@ -32,69 +27,79 @@ namespace CryptoVisor.Application.Services
             var coinHistoriesDaily = coinHistories.Where(x => x.Date.Hour == 0).ToList();
 
 
-            await GetRelativeStrengthIndex(coinHistoriesDaily);
+            await GetRelativeStrengthIndexList(coinHistoriesDaily);
 
             return new OhlcStatitical();
         }
 
         private async Task GetExponentialMovingAverage(IEnumerable<OhlcCoinHistory> coinHistories)
-		{
-			var daysPeriod = coinHistories.Count();
-			var initialMME = coinHistories.Average(x => x.Close);
-			var multiplicator = 2 / (daysPeriod + 1);
+        {
+            var daysPeriod = coinHistories.Count();
+            var initialMME = coinHistories.Average(x => x.Close);
+            var multiplicator = 2 / (daysPeriod + 1);
 
-		}
+        }
 
-		private async Task<double> GetBollingerBands()
-		{
-			return 0.0;
-		}
+        private async Task<double> GetBollingerBands()
+        {
+            return 0.0;
+        }
 
-		private async Task<double> GetRelativeStrengthIndex(IEnumerable<OhlcCoinHistory> coinHistories)
-		{
-			//List<double> closes = coinHistories.Select(x => x.Close).ToList();
-			List<double> closes = new List<double>{1010.0, 1010.0,
-										1005.0,
-										1005.0,
-										1010.0,
-										1005.0,
-										1010.0,
-										1005.0,
-										1010.0,
-										1005.0,
-										1010.0,
-										1005.0,
-										1010.0,
-										1005.0 };
+        private async Task<List<RelativeStrengthIndex>> GetRelativeStrengthIndexList(IEnumerable<OhlcCoinHistory> coinHistories)
+        {
+            var listOfRsiPeriods = new List<RelativeStrengthIndex>();
 
+            var orderedCoinHistories = coinHistories.OrderByDescending(x => x.Date).ToList();
+
+            for (int i = 0; i <= orderedCoinHistories.Count - 14; i++)
+            {
+                var periodCoinList = orderedCoinHistories.Skip(i).Take(14).Select(x => x.Close).ToList();
+                var rsi = await GetRelativeStrengthIndex(periodCoinList);
+
+                var rsiResult = new RelativeStrengthIndex
+                {
+                    Date = orderedCoinHistories.Skip(i).Select(x => x.Date).FirstOrDefault(),
+                    Value = rsi
+                };
+
+                listOfRsiPeriods.Add(rsiResult);
+            }
+
+            return listOfRsiPeriods;
+        }
+
+        private async Task<double> GetRelativeStrengthIndex(List<double> closes)
+        {
             var period = closes.Count();
 
-			List<double> diaryVariation = new List<double>();
-			for (int i = 1; i < closes.Count; i++)
-			{
-				diaryVariation.Add(closes.Skip(i).FirstOrDefault() - closes.Skip(i - 1).FirstOrDefault());
-			}
+            List<double> diaryVariation = new List<double>();
+            for (int i = 1; i < closes.Count; i++)
+            {
+                diaryVariation.Add(closes.Skip(i).FirstOrDefault() - closes.Skip(i - 1).FirstOrDefault());
+            }
 
-			List<double> gains = [];
-			List<double> losses = [];
-			foreach (var change in diaryVariation)
-			{
-				gains.Add(Math.Max(0, change));
-				losses.Add(Math.Max(0, -change));
-			}
+            List<double> gains = [];
+            List<double> losses = [];
+            foreach (var change in diaryVariation)
+            {
+                gains.Add(Math.Max(0, change));
+                losses.Add(Math.Max(0, -change));
+            }
 
-			double avgGain = gains.GetRange(0, period - 1).Sum() / period;
-			double avgLoss = losses.GetRange(0, period - 1).Sum() / period;
+            double avgGain = gains.GetRange(0, period - 1).Sum() / period;
+            double avgLoss = losses.GetRange(0, period - 1).Sum() / period;
 
-			for (int i = period; i < gains.Count; i++)
-			{
-				avgGain = ((avgGain * (period - 1)) + gains[i]) / period;
-				avgLoss = ((avgLoss * (period - 1)) + losses[i]) / period;
-			}
+            for (int i = period; i < gains.Count; i++)
+            {
+                avgGain = ((avgGain * (period - 1)) + gains[i]) / period;
+                avgLoss = ((avgLoss * (period - 1)) + losses[i]) / period;
+            }
 
-			double rs = avgGain / avgLoss;
-			double rsi = 100 - (100 / (1 + rs));
-			return rsi;
-		}
-	}
+            double rs = avgGain / avgLoss;
+            double rsi = 100 - (100 / (1 + rs));
+
+            return rsi;
+        }
+
+    }
 }
